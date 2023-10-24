96e9985b7c5331cea5dd7381afa44bdfb314c2df
Andre Proenca <andreproenca@hotmail.com>
2023-10-24 15:07:46 -0400

    commit

---
+++ src/Heroi.java
@@ -0,0 +1,13 @@
+public class Heroi {
+    
+String nome;
+int idade;
+String tipo;
+
+public void Heroi(String nome, int idade, String tipo){
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+}
+
+}
